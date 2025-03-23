3efbbe094c5884714641bea6b74347b5ed20893f
paogabivilla <pagavi161179@gmail.com>
2025-03-22 21:19:09 -0300

    Validación

--- src/routes/superHeroRoutes.mjs
+++ src/routes/superHeroRoutes.mjs
@@ -11,14 +11,20 @@ import {
     eliminarSuperheroePorNombreController
 } from '../controllers/superheroesController.mjs';
 
+import { validationDataSuperHeros } from '../middlewares/validationRules.mjs';
+import { handleValidationErrors } from '../middlewares/errorMiddleware.mjs';
+
 const router = express.Router();
+
 router.get('/heroes/menores-30', obtenerSuperheroesMenoresDe30Controller);
 router.get('/heroes/mayores-30', obtenerSuperheroesMayoresDe30Controller);
 router.get('/heroes', obtenerTodosLosSuperheroesController);
 router.get('/heroes/buscar/:atributo/:valor', buscarSuperheroesPorAtributoController);
-router.get('/heroes/:id', obtenerSuperheroePorIdController);
-router.post('/heroes/crear', crearNuevoSuperheroeController);
-router.put('/heroes/actualizar/:id', actualizarSuperheroeController);
+router.get('/heroes/:id', validationDataSuperHeros, handleValidationErrors, obtenerSuperheroePorIdController);
+
+//sprint 3 - TP1 - TP2
+router.post('/heroes/crear', validationDataSuperHeros(), handleValidationErrors, crearNuevoSuperheroeController);
+router.put('/heroes/actualizar/:id', validationDataSuperHeros(), handleValidationErrors,  actualizarSuperheroeController);
 router.delete('/heroes/eliminar/id/:id', eliminarSuperheroePorIdController);
 router.delete('/heroes/eliminar/nombre/:nombre', eliminarSuperheroePorNombreController);
 
